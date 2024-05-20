936573a3835f934d3eb1dd5595fc9b6747f19c6a
arif2004js <yearif689@gmail.com>
2024-05-20 15:16:23 +0400

    express 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 410;
+const port = 4010;
 const cards = [
   {id: 1, name: 'card1', description: 'card1 description', price: 100},
   {id: 2, name: 'card2', description: 'card2 description', price: 200},  
@@ -10,10 +10,27 @@ const cards = [
 
 
 ]
+const products = [
+  {id: 1, name: 'product1', description: 'product1 description', price: 100},
+  {id: 2, name: 'product2', description: 'product2 description', price: 200},
+  {id: 3, name: 'product3', description: 'product3 description', price: 300},
+  {id: 4, name: 'product4', description: 'product4 description', price: 400},
+  {id: 5, name: 'product5', description: 'product5 description', price: 500},
+  {id: 6, name: 'product6', description: 'product6 description', price: 600},
+  {id: 7, name: 'product7', description: 'product7 description', price: 700},
+  {id: 8, name: 'product8', description: 'product8 description', price: 800},
+  {id: 9, name: 'product9', description: 'product9 description', price: 900},
+  {id: 10, name: 'product10', description: 'product10 description', price: 1000},
+
+]
 app.listen(port, function () {
     console.log(`Example app listening on port ${port} !`);
   });
 
   app.get('/cards/:id', function (req, res) {
     res.send(JSON.stringify(cards.find((card) => card.id == req.params.id)));
+    
+  });
+  app.get('/products/:id', function (req, res) {
+      res.send(JSON.stringify(products.find((product) => product.id == req.params.id)));
   });
